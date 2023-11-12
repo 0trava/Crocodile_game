874c0ffdf01e8dd762abcfe5fc350a7fae708b56
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-11-12 18:27:37 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,32 @@
 import card from './flashcards.json';
 import style from './App.module.css';
+import { useState } from 'react';
 
 export const App = () => {
-  console.log(card);
+  const [randomCard, setRandomCard] = useState(null);
+
+  const pickRandomCard = () => {
+    const randomIndex = Math.floor(Math.random() * card.images.length);
+    const selectedCard = card.images[randomIndex];
+    setRandomCard(selectedCard);
+  };
 
 
   return (
     <div className={style.container}>
       <h1>Crocodile</h1>
-       
+        
 
 
        <div className={style.card}>
-          <img src={card.images[0]} alt="card" />
+          {randomCard && (
+          <img src={randomCard} alt="card" />
+          )}
        </div>
+
+       <button onClick={pickRandomCard}>
+        <span class={style.text}>Lets go...</span>
+      </button>
     </div>
   );
 };
