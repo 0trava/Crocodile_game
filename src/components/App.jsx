c4aedc22b2f1c4639ff1f008ef2849ccc6ddb2cf
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-11-14 18:20:33 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,26 +39,53 @@ export const App = () => {
 
       </div>
 
+      
+      <div className={style.block}>
+      {/* Help words */}
+      {category === card.animal ?
+      <div className={style.help_words}>
+        {/* Animal */}
+        <h3>Hint words - Слова підказки</h3>
+        <p><span>Carnivore/herbivore/omnivore</span> -  Хижак/травоїдне/всеїдне</p>
+        <p><span>Fluffy/slippery/soft/</span> – пухнастий/слизький/м’який</p>
+        <p><span>Spotty/stripy</span> – плямистий/смугастий</p>
+        <p><span>Fur/feathers/scales/beak</span> - Шерсть/перо/луска/дзьоб </p>
+        <p><span>Tail/wings/mustache/paws/hooves</span>  - хвіст/крила/вуса/лапи/копита</p>
+
+
+        <button onClick={pickRandomCard}>
+          <span className={style.text}>Lets go...</span>
+        </button>
+      </div> : "" }
+
+
+
+
+      {/* Cards show */}
+        <div>
+        <div className={style.card}>
+            {category === card.jobs && <h3 className={style.title_card}>Jobs</h3>}
+            {category === card.animal && <h3 className={style.title_card}>Animal</h3>}
+            {category === card.fruits && <h3 className={style.title_card}>Fruits</h3>}
+
+            {randomCard && (
+            <img src={randomCard} alt="card" />
+            )}
+        </div>
+
+
+        </div>
+      </div>
+      
+
 
         
 
 
-       <div className={style.card}>
-          {category === card.jobs && <h3 className={style.title_card}>Jobs</h3>}
-          {category === card.animal && <h3 className={style.title_card}>Animal</h3>}
-          {category === card.fruits && <h3 className={style.title_card}>Fruits</h3>}
-
-          {randomCard && (
-          <img src={randomCard} alt="card" />
-          )}
-       </div>
-
-       <button onClick={pickRandomCard}>
-        <span className={style.text}>Lets go...</span>
-      </button>
 
 
       {/* Image */}
+
 
       <img className={style.image} src={crocodileImage} alt="crocodile" />
     </div>
