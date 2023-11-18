df77abe5ccf3dc50ed0faa37768ac2533251957d
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-11-18 14:08:28 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,9 +42,10 @@ export const App = () => {
       
       <div className={style.block}>
       {/* Help words */}
+
+      {/* Animal */}
       {category === card.animal ?
       <div className={style.help_words}>
-        {/* Animal */}
         <h3>Hint words - Слова підказки</h3>
         <p><span>Carnivore/herbivore/omnivore</span> -  Хижак/травоїдне/всеїдне</p>
         <p><span>Fluffy/slippery/soft/</span> – пухнастий/слизький/м’який</p>
@@ -52,6 +53,36 @@ export const App = () => {
         <p><span>Fur/feathers/scales/beak</span> - Шерсть/перо/луска/дзьоб </p>
         <p><span>Tail/wings/mustache/paws/hooves</span>  - хвіст/крила/вуса/лапи/копита</p>
 
+
+        <button onClick={pickRandomCard}>
+          <span className={style.text}>Lets go...</span>
+        </button>
+      </div> : "" }
+
+        {/* Fruits */}
+        {category === card.fruits ?
+        <div className={style.help_words}>
+        <div>
+        <h3>Hint words - Слова підказки</h3>
+        <p><span>Sweet and sour/sweet/sour</span> -  Солодкий-кислий/солодкий/кислий</p>
+        <p><span>Твердий/м'який</span> – hard/soft</p>
+        <p><span>Гладенький/жорсткий</span> – Smooth/hard</p>
+        <p><span>Grow in the tropics/ in the garden</span> - Ростуть в тропіках/ в саду </p>
+        </div>
+
+        <button onClick={pickRandomCard}>
+          <span className={style.text}>Lets go...</span>
+        </button>
+      </div> : "" }
+
+        {/* Jobs */}
+        {category === card.jobs ?
+        <div className={style.help_words}>
+        <h3>Hint words - Слова підказки</h3>
+        <p><span>Робота безпечна/ небезпечна</span> -  The work is safe/dangerous</p>
+        <p><span>Треба носити форму</span> – Must wear a uniform</p>
+        <p><span>Лікує/обслуговує/продає/рятує/контролює</span> – Heals/maintains/sells/rescues/controls</p>
+        <p><span>Працює в цирку/офісі/на свіжому повітрі/лікарні</span> - Works in the circus/office/outdoors/hospital </p>
 
         <button onClick={pickRandomCard}>
           <span className={style.text}>Lets go...</span>
